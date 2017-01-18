8bbeae1aca27f179e09d9a3a0d1ba95571c71fe3
Jan Carlson <jan.carlson@mdh.se>
2017-01-18 11:50:56 +0100

    Context model updated

--- COACH/context_model/ContextModelService.py
+++ COACH/context_model/ContextModelService.py
@@ -29,12 +29,32 @@ class ContextModelService(coach.Microservice):
         """
         Endpoint which lets the user edit context information.
         """
-        context_text = get_service(case_db, "get_case_property", case_id = case_id, name = "context_text")
-        return render_template("edit_context_dialogue.html", context_text = context_text)     
-
         
+        context_data = {}
+        context_data['general'] = get_service(case_db, "get_case_property", case_id = case_id, name = "context_general")
+        context_data['organization'] = get_service(case_db, "get_case_property", case_id = case_id, name = "context_organization")
+        context_data['product'] = get_service(case_db, "get_case_property", case_id = case_id, name = "context_product")
+        context_data['stakeholders'] = get_service(case_db, "get_case_property", case_id = case_id, name = "context_stakeholders")
+        context_data['methodandtechnology'] = get_service(case_db, "get_case_property", case_id = case_id, name = "context_methodandtechnology")
+        context_data['marketandbusiness'] = get_service(case_db, "get_case_property", case_id = case_id, name = "context_marketandbusiness")
+        
+        return render_template("edit_context_dialogue.html", context_data = context_data)     
+
+
+
+
     @endpoint("/edit_context", ["POST"])
-    def edit_context(self, case_db, case_id, context_text):
+    def edit_context(self, case_db, case_id, 
+                     context_general_text,
+                     context_organization_text,
+                     context_product_text,
+                     context_stakeholders_text,
+                     context_methodandtechnology_text,
+                     context_marketandbusiness_text):
+
+    # Alternative:
+    # def edit_context(self, case_db, case_id, context_general_text, context_organization_text, **kwargs):
+
         """
         This method is called using POST when the user presses the save button in the edit_context_dialogue_transition.
         It gets several form parameters: 
@@ -43,8 +63,21 @@ class ContextModelService(coach.Microservice):
         It writes the new context information to the database, and then returns a status message to be shown in the main dialogue window.
         """
         # Write the new context information to the database.
-        post_service(case_db, "change_case_property", case_id = str(case_id), name = "context_text", value = context_text)
-        return "Context information saved. (" + context_text + ")"
+        post_service(case_db, "change_case_property", case_id = str(case_id), name = "context_general", value = context_general_text)
+        post_service(case_db, "change_case_property", case_id = str(case_id), name = "context_organization", value = context_organization_text)
+        post_service(case_db, "change_case_property", case_id = str(case_id), name = "context_product", value = context_product_text)
+        post_service(case_db, "change_case_property", case_id = str(case_id), name = "context_stakeholders", value = context_stakeholders_text)
+        post_service(case_db, "change_case_property", case_id = str(case_id), name = "context_methodandtechnology", value = context_methodandtechnology_text)
+        post_service(case_db, "change_case_property", case_id = str(case_id), name = "context_marketandbusiness", value = context_marketandbusiness_text)
+        
+        # Alternative:
+        # post_service(case_db, "change_case_property", case_id = str(case_id), name = "context_product", value = kwargs['context_product_text'])
+        
+    
+        return "Context information saved."
+    
+    
+    
     
 if __name__ == '__main__':
     ContextModelService(sys.argv[1]).run()
