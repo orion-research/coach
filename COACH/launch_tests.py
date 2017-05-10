f1870a16a64f79b7d1a6709fe6c93aef2f49822b
Markus Borg <markus.borg@gmail.com>
2017-05-10 18:29:41 +0200

    Added test case create random user

--- COACH/launch_tests.py
+++ COACH/launch_tests.py
@@ -8,6 +8,8 @@ Make sure to close all running instances of COACH before running the tests.
 """
 
 import unittest
+import random
+import string
 from COACH import launch_local
 from COACH.framework import coach
 
@@ -15,7 +17,8 @@ class TestCreateUser(unittest.TestCase):
     int_service_proxy = None
     dummyUser = None
     url = "http://127.0.0.1:5000" #use 127.0.0.1:443 for remote server
-    
+
+
     def setUp(self):
         launch_local.run_all()
         self.int_service_proxy = coach.Proxy(self.url, ["POST", "GET"])
@@ -24,15 +27,29 @@ class TestCreateUser(unittest.TestCase):
     def test_get_version(self):
         self.assertNotEqual(self.int_service_proxy.get_version(), "No version information available", "Invalid version number")
 
-        
+
     def test_create_user_wrong_repeated_password(self):
-        http_response = self.int_service_proxy.create_user(user_id = "Test", password1 = "pw1", password2 = "pw2", name = "Full Name", email = "markus.borg@ri.se")
+        http_response = self.int_service_proxy.create_user(user_id = "Test", password1 = "pw1", password2 = "pw2", name = "Full Name", email = "orion@ri.se")
         self.assertIn("Error: Password and repeated password not equal!", http_response, "COACH did not notice wrong repeated password")
 
-        
+
+    def test_create_random_user(self):
+        password = self.get_random_string(8)
+        http_response = self.int_service_proxy.create_user(user_id = self.get_random_string(6),
+                                                           password1 = password,
+                                                           password2 = password,
+                                                           name = self.get_random_string(10),
+                                                           email = self.get_random_string(5) + "@ri.se")
+        self.assertIn("To validate your COACH user identity", http_response, "COACH did accept creation of new user")
+
+
     def tearDown(self):
         print("Tear down")
-    
+
+    def get_random_string(self, length):
+        return "".join([random.choice(string.ascii_lowercase + string.ascii_uppercase + string.digits)
+                        for _ in range(0, length)])
+
 
 if __name__ == '__main__':
     unittest.main()
